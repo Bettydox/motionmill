5ce933e7b4cb4c74a64fb01dabf96a265c796d59
MaartenMenten <maarten@motionmill.com>
2014-01-08 10:58:38 +0100

    motionmill settings: finetuning

--- plugins/motionmill-settings/js/scripts.js
+++ plugins/motionmill-settings/js/scripts.js
@@ -1,38 +1,50 @@
 (function($, window, document, undefined)
 {
+	$.extend(MM_Settings,
+	{
+		elem : null,
+
+		initialize : function()
+		{
+			this.elem = $( '.' + this.page_hook );
+
+			// colorpicker
+			this.elem.find('.colorpicker').each(function()
+			{
+				var color = new Color( $(this).val() );
+
+				$(this)
+					.data('set', false)
+					.css( 'color', color.getMaxContrastColor().toString() )
+			    	.css( 'background-color', color.toString() )
+			    	.attr( 'readonly', 'readonly' )
+					.iris(
+					{
+						hide: true,
+						change: function(event, ui)
+						{
+			        		$(this).css( 'color', ui.color.getMaxContrastColor().toString() );
+			    			$(this).css( 'background-color', ui.color.toString() );
+			    		}
+					})
+					.click(function(e)
+					{
+						$(this).iris('show');
+
+						e.stopPropagation();
+					})
+			});
+
+			$('body').click(function(e)
+			{
+				MM_Settings.elem.find('.colorpicker').iris('hide');
+			});
+		}
+	})
+
 	$(document).ready(function()
 	{
-		// colorpicker
-		$('.wrap .colorpicker').each(function()
-		{
-			var color = new Color( $(this).val() );
-
-			$(this)
-				.data('set', false)
-				.css( 'color', color.getMaxContrastColor().toString() )
-		    	.css( 'background-color', color.toString() )
-		    	.attr( 'readonly', 'readonly' )
-				.iris(
-				{
-					hide: true,
-					change: function(event, ui)
-					{
-		        		$(this).css( 'color', ui.color.getMaxContrastColor().toString() );
-		    			$(this).css( 'background-color', ui.color.toString() );
-		    		}
-				})
-				.click(function(e)
-				{
-					$(this).iris('show');
-
-					e.stopPropagation();
-				})
-		});
-	
-		$('body').click(function(e)
-		{
-			$('.wrap .colorpicker').iris('hide');
-		});
+		MM_Settings.initialize();
 	});
 
 })(jQuery, window, document);
--- plugins/motionmill-settings/motionmill-settings.php
+++ plugins/motionmill-settings/motionmill-settings.php
@@ -44,9 +44,8 @@ if ( ! class_exists('MM_Settings') )
 			add_action( 'admin_init', array(&$this, 'on_admin_init') );
 			add_action( 'admin_menu', array(&$this, 'on_admin_menu') );
 			add_action( 'admin_enqueue_scripts', array(&$this, 'on_admin_enqueue_scripts') );
-
 			add_filter( 'motionmill_page_slug', array(&$this, 'on_motionmill_page_slug') );
-
+			
 			register_deactivation_hook( MM_FILE, array(&$this, 'on_deactivate') );
 		}
 
@@ -56,7 +55,7 @@ if ( ! class_exists('MM_Settings') )
 
 			if ( $page )
 			{
-				$options = get_option( $page['option_name'], $this->get_default_options( $page['id'] ) );
+				$options = get_option( $page['option_name'], $this->get_default_options($page['id']) );
 
 				if ( $name == null )
 				{
@@ -91,7 +90,7 @@ if ( ! class_exists('MM_Settings') )
 
 		public function on_init()
 		{
-			// pages
+			// registers pages
 			foreach ( apply_filters( $this->page_slug . '_pages', array() ) as $data )
 			{
 				if ( ! is_array($data) || empty($data['id']) )
@@ -102,11 +101,11 @@ if ( ! class_exists('MM_Settings') )
 					'id'          => $data['id'],
 					'title'       => $data['id'],
 					'description' => '',
-					'option_name' => stripos( $data['id'] , 'motionmill_' ) !== 0 ? 'motionmill_' . $data['id'] : $data['id'] // makes sure the option name has following format: 'motionmill_{page_id}'
+					'option_name' => stripos( $data['id'] , 'motionmill_' ) !== 0 ? 'motionmill__' . $data['id'] : $data['id'] // makes sure the option name has following format: {page_slug}_{page_id}
 				), $data);
 			}
 
-			// sections
+			// registers sections
 			foreach ( apply_filters( $this->page_slug . '_sections', array() ) as $data )
 			{
 				if ( ! is_array($data) || empty($data['id']) )
@@ -121,10 +120,10 @@ if ( ! class_exists('MM_Settings') )
 				), $data);
 			}
 
-			// fields
+			// registers fields
 			foreach ( apply_filters( $this->page_slug . '_fields', array() ) as $data )
 			{
-				if ( ! is_array($data) || empty($data['id']) || empty($data['page']) )
+				if ( ! is_array($data) || empty($data['id']) )
 					continue;
 
 				$this->fields[] = array_merge(array
@@ -134,8 +133,8 @@ if ( ! class_exists('MM_Settings') )
 					'description' => '',
 					'type'        => 'textfield',
 					'value'       => '',
-					'page'        => $data['page'],
-					'section'     => $data['section']
+					'page'        => '',
+					'section'     => ''
 				), $data);
 			}
 		}
@@ -156,6 +155,7 @@ if ( ! class_exists('MM_Settings') )
 				}
 			}
 
+			// registers a setting for each page
 			foreach ( $this->pages as $page )
 			{
 				register_setting( $page['id'], $page['option_name'], array(&$this, 'on_sanitize_input') );
@@ -163,19 +163,181 @@ if ( ! class_exists('MM_Settings') )
 			
 			foreach ( $this->sections as $section )
 			{
-				add_settings_section( $section['id'], $section['title'], create_function('$a', 'echo "' . $section['description'] . '";'), $section['page'] );
+				$callback = create_function( '$a', 'echo "' . $section['description'] . '";' );
+
+				add_settings_section( $section['id'], $section['title'], $callback, $section['page'] );
 			}
 
 			foreach ( $this->fields as $field )
 			{
 				$page = MM_Helper::get_element_by( 'id='.$field['page'], $this->pages );
 
-				add_settings_field( $field['id'], $field['title'], array(&$this, 'form_' . $field['type']), $field['page'], $field['section'], array_merge($field, array
+				if ( is_array($field['type']) )
+				{
+					$callback = $field['type'];
+				}
+
+				add_settings_field( $field['id'], $field['title'], array(&$this, 'on_print_field'), $field['page'], $field['section'], array_merge($field, array
 				(
+					'id'        => $field['id'],
 					'label_for' => $field['id'],
 					'name'      => $page['option_name'] . '[' . $field['id'] . ']',
 					'value'     => $this->get_option( $field['page'], $field['id'] )
 				)));
+			}
+		}
+
+		public function on_print_field($field)
+		{
+			switch ( $field['type'] )
+			{
+				case 'textfield':
+
+					$options = array_merge(array
+					(
+						'id'    => '',
+						'class' => 'regular-text',
+						'name'  => '',
+						'value' => '',
+						'extra' => ''
+					), $field);
+
+					printf( '<input type="text" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
+					
+					break;
+
+				case 'password':
+
+					$options = array_merge(array
+					(
+						'id'    => '',
+						'class' => 'regular-text',
+						'name'  => '',
+						'value' => '',
+						'extra' => ''
+					), $field);
+
+					printf( '<input type="password" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
+					
+					break;
+
+				case 'colorpicker':
+
+					$options = array_merge(array
+					(
+						'id'    => '',
+						'class' => 'regular-text colorpicker',
+						'name'  => '',
+						'value' => '',
+						'extra' => ''
+					), $field);
+
+					printf( '<input type="text" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
+					
+					break;
+
+				case 'checkbox':
+
+					$options = array_merge(array
+					(
+						'id'    => '',
+						'class' => '',
+						'name'  => '',
+						'value' => '',
+						'extra' => ''
+					), $field);
+
+					$options['extra'] .= checked( $options['value'], 1, false );
+
+					printf( '<input type="checkbox" id="%s" class="%s" name="%s" value="1" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), $options['extra'] );
+					
+					break;
+
+				case 'textarea':
+
+					$options = array_merge(array
+					(
+						'id'    => '',
+						'class' => 'large-text',
+						'name'  => '',
+						'value' => '',
+						'cols'  => 50,
+						'rows'  => get_option( 'default_post_edit_rows', 10 ),
+						'extra' => ''
+					), $field);
+
+					printf( '<textarea id="%s" class="%s" name="%s" cols="%s" rows="%s" %s>%s</textarea>', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['cols']), esc_attr($options['rows']), esc_html($options['extra']), esc_textarea($options['value']) );
+					
+					break;
+
+				case 'editor':
+
+					$options = array_merge(array
+					(
+						'id' 		  	=> '',
+						'class' 	  	=> '',
+						'name'       	=> '',
+						'value'       	=> '',
+						'rows'        	=> get_option( 'default_post_edit_rows', 10 ),
+						'wpautop' 		=> true,
+						'media_buttons' => true,
+						'editor_css' 	=> '',
+						'teeny' 		=> false,
+						'dfw' 			=> false,
+						'tinymce' 		=> true,
+						'quicktags' 	=> true,
+						'description'   => ''
+					), $field);
+
+					wp_editor( $options['value'], $options['id'], array
+					(
+						'wpautop' 		=> $options['wpautop'],
+						'media_buttons' => $options['media_buttons'],
+						'textarea_name' => $options['name'],
+						'textarea_rows' => $options['rows'],
+						'editor_css' 	=> $options['editor_css'],
+						'editor_class' 	=> $options['class'],
+						'teeny' 		=> $options['teeny'],
+						'dfw' 			=> $options['dfw'],
+						'tinymce' 		=> $options['tinymce'],
+						'quicktags' 	=> $options['quicktags']
+					));
+
+					break;
+
+				case 'dropdown':
+
+					$options = array_merge(array
+					(
+						'id'          => '',
+						'class'       => '',
+						'name'        => '',
+						'options'     => array(),
+						'value'       => '',
+						'extra'       => '',
+						'description' => ''
+					), (array) $args);
+
+					printf( '<select id="%s" class="%s" name="%s" %s>', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), $options['extra'] );
+
+					foreach ( $options['options'] as $key => $value )
+					{
+						printf( '<option value="%s"%s>%s</option>', esc_attr($key), selected( $key, $options['value'], false ), esc_html($value) );
+					}
+
+					print( '</select>' );
+
+					break;
+				
+				default:
+
+					do_action( $this->page_slug . '_print_field_type_' . $field['type'], $field );
+			}
+
+			// description
+			if ( $field['description'] != '' )
+			{
+				printf( '<p class="description">%s</p>', $field['description'] );
 			}
 		}
 		
@@ -191,15 +353,20 @@ if ( ! class_exists('MM_Settings') )
 			if ( $screen->id != $this->page_hook )
 				return;
 
+			if ( ! $this->current_page )
+				return;
+			
 			// colorpicker
 			wp_enqueue_script( 'iris' );
 
 			// general
 			wp_enqueue_style( $this->page_slug . '-style', plugins_url('css/style.css', __FILE__), null, '1.0.0', 'all' );
 			wp_enqueue_script( $this->page_slug . '-scripts',  plugins_url('js/scripts.js', __FILE__), array('jquery'), '1.0.0', true );
-
-			if ( ! $this->current_page )
-				return;
+			wp_localize_script( $this->page_slug . '-scripts', 'MM_Settings', array
+			(
+				'page_hook' => $this->page_hook,
+				'page_slug' => $this->page_slug
+			));
 
 			do_action( $this->page_slug . '_enqueue_scripts', $this->current_page['id'] );
 		}
@@ -235,7 +402,7 @@ if ( ! class_exists('MM_Settings') )
 					<?php settings_fields( $this->current_page['id'] ); ?>
 
 					<?php echo $this->current_page['description']; ?>
-
+					
 					<?php do_settings_sections( $this->current_page['id'] ); ?>
 
 					<?php submit_button(); ?>
@@ -246,8 +413,6 @@ if ( ! class_exists('MM_Settings') )
 				<?php endif; ?>
 
 			</div><!-- .wrap-->
-
-			
 
 			<?php
 		}
@@ -265,182 +430,11 @@ if ( ! class_exists('MM_Settings') )
 			return apply_filters( $this->page_slug . '_sanitize_input', $input, $page_id );
 		}
 
-		public function on_sanitize_input_field($input, $method, $field)
-		{
-			switch ( $method )
-			{
-				case 'trim' :
-					return trim($input);
-			}
-
-			return $input;
-		}
-
 		public function on_deactivate()
 		{
 			foreach ( $this->pages as $page )
 			{
 				unregister_setting( $page['id'], $page['option_name'], array(&$this, 'on_sanitize_input') );
-			}
-		}
-
-		public function form_dropdown($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id'          => '',
-				'class'       => '',
-				'name'        => '',
-				'options'     => array(),
-				'value'       => '',
-				'extra'       => '',
-				'description' => ''
-			), (array) $args);
-
-			printf( '<select id="%s" class="%s" name="%s" %s>', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), $options['extra'] );
-
-			foreach ( $options['options'] as $key => $value )
-			{
-				printf( '<option value="%s"%s>%s</option>', esc_attr($key), selected( $key, $options['value'], false ), esc_html($value) );
-			}
-
-			print( '</select>' );
-
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $field['description'] );
-			}
-		}
-
-		public function form_textfield($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id'          => '',
-				'class'       => 'regular-text',
-				'name'        => '',
-				'value'       => '',
-				'extra'       => '',
-				'description' => ''
-			), (array) $args);
-
-			printf( '<input type="text" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
-		
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $options['description'] );
-			}
-		}
-
-		public function form_password($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id'          => '',
-				'class'       => 'regular-text',
-				'name'        => '',
-				'value'       => '',
-				'extra'       => '',
-				'description' => ''
-			), (array) $args);
-
-			printf( '<input type="password" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
-			
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $options['description'] );
-			}
-		}
-
-		public function form_checkbox($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id'          => '',
-				'class'       => '',
-				'name'        => '',
-				'value'       => '',
-				'extra'       => '',
-				'description' => ''
-			), (array) $args);
-
-			$options['extra'] .= checked( isset($args['value']) && $args['value'] == '1', '1', false );
-			$options['value'] = '1';
-
-			printf( '<input type="checkbox" id="%s" class="%s" name="%s" value="%s" %s />', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['value']), $options['extra'] );
-			
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $options['description'] );
-			}
-		}
-
-		public function form_colorpicker($args = array())
-		{
-			$this->form_textfield(array_merge($args, array
-			(
-				'class' => 'regular-text colorpicker'
-			)));
-		}
-
-		public function form_textarea($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id'          => '',
-				'class'       => 'large-text',
-				'name'        => '',
-				'value'       => '',
-				'extra'       => '',
-				'cols'        => '50',
-				'rows'        => get_option('default_post_edit_rows', 10),
-				'description' => ''
-			), (array) $args);
-
-			printf( '<textarea id="%s" class="%s" name="%s" cols="%s" rows="%s" %s>%s</textarea>', esc_attr($options['id']), esc_attr($options['class']), esc_attr($options['name']), esc_attr($options['cols']), esc_attr($options['rows']), esc_html($options['extra']), esc_textarea($options['value']) );
-			
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $options['description'] );
-			}
-		}
-
-		public function form_editor($args = array())
-		{
-			$options = array_merge(array
-			(
-				'id' 		  	=> '',
-				'class' 	  	=> '',
-				'name'       	=> '',
-				'value'       	=> '',
-				'rows'        	=> get_option('default_post_edit_rows', 10),
-				'wpautop' 		=> true,
-				'media_buttons' => true,
-				'editor_css' 	=> '',
-				'teeny' 		=> false,
-				'dfw' 			=> false,
-				'tinymce' 		=> true,
-				'quicktags' 	=> true,
-				'description'   => ''
-			), $args);
-
-			wp_editor( $options['value'], $options['id'], array
-			(
-				'wpautop' 		=> $options['wpautop'],
-				'media_buttons' => $options['media_buttons'],
-				'textarea_name' => $options['name'],
-				'textarea_rows' => $options['rows'],
-				'editor_css' 	=> $options['editor_css'],
-				'editor_class' 	=> $options['class'],
-				'teeny' 		=> $options['teeny'],
-				'dfw' 			=> $options['dfw'],
-				'tinymce' 		=> $options['tinymce'],
-				'quicktags' 	=> $options['quicktags']
-			));
-
-			if ( $options['description'] != '' )
-			{
-				printf( '<p class="description">%s</p>', $options['description'] );
 			}
 		}
 	}
