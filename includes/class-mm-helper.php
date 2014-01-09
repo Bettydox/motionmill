e81b693fd6ce3a9ebae53a9194706abb64b80837
MaartenMenten <maarten@motionmill.com>
2014-01-09 14:38:48 +0100

    added helper get_image_size

--- includes/class-mm-helper.php
+++ includes/class-mm-helper.php
@@ -123,6 +123,35 @@ class MM_Helper
 
 		return strlen($string) > 0 ? explode($delimiter, $string) : array();
 	}
+
+	public static function get_image_size($url)
+	{
+		// alternative to getimagesize (uses fopen)
+
+		$response = wp_remote_get($url);
+
+		if ( is_wp_error( $response ) )
+		{
+			trigger_error( 'Unable to load image: ' . $response->get_message() );
+
+			return null;
+		}
+
+		$img = imagecreatefromstring( $response['body'] );
+
+		if ( ! $img )
+		{
+			trigger_error( 'Unable to get image sizes.' );
+
+			return null;
+		}
+
+		return array
+		(
+			'width'  => imagesx($img),
+			'height' => imagesy($img)
+		);
+	}
 }
 
 ?>
