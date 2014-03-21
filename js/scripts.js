d638f3e15d7fd946f7103f587a1c086233cff0fa
Maarten <maarten@motionmill.com>
2014-03-21 11:09:16 +0100

    simplified main javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,9 @@
 (function($)
 {
-	$.ajaxSetup(
-	{
-		cache 	 : false,
-		dataType : 'json'
-	});
-
-	$.extend(Motionmill,
-	{
-		initialize : function()
-		{
-			
-		}
-	});
-
-	Motionmill.initialize();
-
 	$(document).ready(function()
 	{
 		$('.hide-if-js').hide();
 		$('.hide-if-no-js').show();
-			
-		Motionmill.initialize();
 	});
 
 })(jQuery);
