aab9adaa0aa062a00d62ee45ad655656ff168b07
Maarten Menten <mentenmaarten@gmail.com>
2014-04-21 19:20:31 +0200

    added isOnScreen() query function

--- plugins/motionmill-assets/js/plugins.js
+++ plugins/motionmill-assets/js/plugins.js
@@ -1,9 +1,9 @@
-(function($)
+(function($, window, document, undefined)
 {
-	$.fn.exists = function(callback)
+	$.fn.exists = function()
 	{
 		return this.length > 0;
-	}
+	};
 
 	$.fn.equalHeight = function( )
 	{
@@ -20,7 +20,7 @@
 		this.css( 'height', maxHeight );
 
 		return this;
-	}
+	};
 
 	$.fn.gotoByScroll = function(target, args)
 	{
@@ -34,6 +34,26 @@
 		$(target).stop().animate( { scrollTop: this.offset().top - options.offset }, options );
 
 		return this;
-	}
+	};
 
-})(jQuery);
+	// http://upshots.org/javascript/jquery-test-if-element-is-in-viewport-visible-on-screen
+	$.fn.isOnScreen = function()
+	{
+	    var win = $(window);
+	    
+	    var viewport = {
+	        top : win.scrollTop(),
+	        left : win.scrollLeft()
+	    };
+
+	    viewport.right = viewport.left + win.width();
+	    viewport.bottom = viewport.top + win.height();
+	    
+	    var bounds = this.offset();
+	    bounds.right = bounds.left + this.outerWidth();
+	    bounds.bottom = bounds.top + this.outerHeight();
+	    
+	    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+    };
+
+})(jQuery, window, document);
