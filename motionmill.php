aeef0aa209595f91cdf1d1ce4bb5f28a5824fbbf
Maarten Menten <mentenmaarten@gmail.com>
2014-04-21 22:26:07 +0200

    Changed version number to 1.3.2

--- motionmill.php
+++ motionmill.php
@@ -4,7 +4,7 @@
  Plugin Name: Motionmill
  Plugin URI: http://motionmill.com
  Description: Motionmill's HQ
- Version: 1.3.1
+ Version: 1.3.2
  Author: Motionmill
  Author URI: http://motionmill.com
  License: GPL2
