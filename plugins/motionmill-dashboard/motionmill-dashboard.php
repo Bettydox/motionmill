4c966842d5b2163399884d43567933d86168647a
Maarten Menten <mentenmaarten@gmail.com>
2014-09-01 00:54:18 +0200

    motionmill dashboard: created plugin instead of included file

--- plugins/motionmill-dashboard/motionmill-dashboard.php
+++ plugins/motionmill-dashboard/motionmill-dashboard.php
@@ -1,21 +1,42 @@
-<?php
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+/*
+------------------------------------------------------------------------------------------------------------------------
+ Plugin Name: Motionmill Dashboard
+ Plugin URI:
+ Description:
+ Version: 1.0.0
+ Author: Maarten Menten
+ Author URI: http://motionmill.com
+ License: GPL2
+------------------------------------------------------------------------------------------------------------------------
+*/
+
+if ( ! class_exists( 'MM_Dashboard' ) )
+{
 	class MM_Dashboard
 	{
+		const FILE = __FILE__;
+
 		protected $motionmill = null;
-
+		
 		public function __construct()
-		{
-			$this->motionmill = Motionmill::get_instance();
-
+		{	
 			add_filter( 'motionmill_settings_pages', array( &$this, 'on_settings_pages' ) );
 			add_filter( 'motionmill_settings_sections', array( &$this, 'on_settings_sections' ) );
+
+			add_action( 'motionmill_init', array( &$this, 'initialize' ) );
+		}
+		
+		public function initialize()
+		{
+			$this->motionmill = Motionmill::get_instance();
+			
 			add_action( 'admin_init', array( &$this, 'on_admin_init' ) );
 		}
 
 		public function on_settings_pages( $pages )
 		{
-			
 			$pages[] = array
 			(
 				'id' 		    => 'motionmill_dashboard',
@@ -48,6 +69,7 @@
 		public function on_admin_init()
 		{
 			add_meta_box( 'motionmill-dashboard-overview', __( 'At a glance', Motionmill::TEXTDOMAIN ), array(&$this, 'on_print_plugins'), 'motionmill_dashboard', 'normal', 'default' );
+			add_meta_box( 'motionmill-dashboard-updates', __( 'Updates', Motionmill::TEXTDOMAIN ), array(&$this, 'on_print_updates'), 'motionmill_dashboard', 'side', 'default' );
 			add_meta_box( 'motionmill-dashboard-docs', __( 'Documentation', Motionmill::TEXTDOMAIN ), array(&$this, 'on_print_documentation'), 'motionmill_dashboard', 'side', 'default' );
 		}
 
@@ -111,7 +133,7 @@
 		{
 			$plugins = array();
 
-			foreach ( $this->motionmill->get_all_plugins() as $file => $plugin )
+			foreach ( $this->motionmill->get_plugins_data( 'intern', 'extern' ) as $file => $plugin )
 			{
 				if ( $plugin['Description'] == '' )
 				{
@@ -139,11 +161,39 @@
 			}
 		}
 
+		public function on_print_updates()
+		{
+			$plugin_manager = $this->motionmill->get_plugin( 'MM_Plugins' );
+
+			$plugins = $plugin_manager->get_plugins_to_update();
+
+			if ( count( $plugins ) == 0 )
+			{
+				_e( 'No updates available.', Motionmill::TEXTDOMAIN );
+			}
+
+			else
+			{
+				print '<ul>';
+
+				foreach ( $plugins as $file )
+				{
+					$data = get_plugin_data( trailingslashit( WP_PLUGIN_DIR ) . $file );
+
+					printf( '<li><h4>%s</h4></li>', $plugin['Name'] );
+				}
+
+				print '</ul>';
+
+				_e( 'Check the <a href="%s">updates section</a> for more information.', '?page=motionmill_plugins', Motionmill::TEXTDOMAIN );
+			}
+		}
+
 		public function on_print_documentation()
 		{
 			$plugins = array();
 
-			foreach ( $this->motionmill->get_all_plugins() as $file => $plugin )
+			foreach ( $this->motionmill->get_plugins_data( 'intern', 'extern' ) as $file => $plugin )
 			{
 				if ( $plugin['PluginURI'] == '' )
 				{
@@ -171,6 +221,19 @@
 			}
 		}
 	}
+}
 
-	$mm_dashboard = new MM_Dashboard();
+// registers plugin
+if ( ! function_exists('motionmill_plugins_add_dashboard') )
+{
+	function motionmill_plugins_add_dashboard( $plugins )
+	{
+		$plugins[] = 'MM_Dashboard';
+
+		return $plugins;
+	}
+
+	add_filter( 'motionmill_plugins', 'motionmill_plugins_add_dashboard' );
+}
+
 ?>
