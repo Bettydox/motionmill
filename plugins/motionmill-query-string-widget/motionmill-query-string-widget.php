b3f5f3235aa393c4f6ec8b8a0695b43fb51a9719
Maarten Menten <mentenmaarten@gmail.com>
2014-09-01 14:51:56 +0200

    updated plugin description

--- plugins/motionmill-query-string-widget/motionmill-query-string-widget.php
+++ plugins/motionmill-query-string-widget/motionmill-query-string-widget.php
@@ -3,7 +3,7 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
  Plugin Name: Motionmill Query String Widget
- Plugin URI: 
+ Plugin URI: https://github.com/addwittz/motionmill/tree/master/plugins/motionmill-query-string-widget
  Description: Shows posts by query string in an editable template.
  Version: 1.0.0
  Author: Maarten Menten
