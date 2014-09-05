882d77cc93fd55fb6ed0ad3cafe78c3be57f3fdf
maartenmenten <maarten@motionmill.com>
2014-09-05 10:57:12 +0200

    config array instead of filters

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php if ( ! defined('ABSPATH') ) exit; // Exits when accessed directly
 
-function motionmill_settings_options( $options )
-{
-	return array_merge( $options, array
+$config = array
+(
+	'MM_Settings' => array
 	(
 		'page_capability'    => 'manage_options',
 		'page_parent_slug'   => 'motionmill',
@@ -11,22 +11,13 @@ function motionmill_settings_options( $options )
 		'page_admin_bar'     => true,
 		'field_rules'        => array( 'trim' ),
 		'field_type'         => 'textfield'
-	));
-}
+	),
 
-add_filter( 'motionmill_settings_options', 'motionmill_settings_options', 5 );
-
-function motionmill_github_options( $options )
-{
-	return array_merge( $options, array
+	'MM_GitHub' => array
 	(
 		'account'       => 'addwittz',
 		'client_login'  => 'mmaarten',
 		'client_secret' => 'e280054b8a4afd585b21f43774b34aa4fb3a0c28',
 		'auth_type'     => 'http_token', // http_password | http_token | url_token
-	));
-}
-
-add_filter( 'motionmill_github_options', 'motionmill_github_options', 5 );
-
-?>
+	)
+);
